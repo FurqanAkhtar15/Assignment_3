dd5a20298b27145d4f117d6f06ab90975adddafb
FurqanAkhtar15 <73681512+FurqanAkhtar15@users.noreply.github.com>
2020-11-06 11:19:16 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Good Morning!!!
-          Whats Up!
+          Whats Up! Halo
         </p>
         <a
           className="App-link"
